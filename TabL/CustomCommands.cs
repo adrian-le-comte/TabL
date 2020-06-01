4aa2113f159b49963037271ad9c9d06c6b5f7d0e
Le Comte Adrian <adrian.le-comte@epita.fr>
2020-06-01 10:42:34 +0200

    First commit of some work done added class in order to duplicate tabs not finished yet

---
+++ TabL/CustomCommands.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Input;
+
+namespace TabL
+{
+    public class CustomCommands
+    {
+        public static readonly RoutedUICommand Exit = new RoutedUICommand
+        (
+            "NewTab",
+            "NewTab",
+            typeof(CustomCommands),
+            new InputGestureCollection()
+            {
+                new KeyGesture(Key.T, ModifierKeys.Control)
+            }
+        );
+    }
+}
